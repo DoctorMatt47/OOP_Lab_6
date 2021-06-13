abfa48cacf50fe3ed5fee9e565b9ebcd1eca2fa2
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:28:55 +0300

    Services - Added statistics parser interface

---
+++ Services/Parsers/IStatsParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OOP_Lab_6.Models;
+
+namespace OOP_Lab_6.Services.StatsParser
+{
+    public interface IStatsParser
+    {
+        Task<IEnumerable<ChampionStats>> GetChampionStatsAsync();
+
+        IEnumerable<ChampionStats> GetChampionStats();
+    }
+}
