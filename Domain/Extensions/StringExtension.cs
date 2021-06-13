e509dfe1cff7a5e13c95de0d7b345593bf271b5a
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:27:49 +0300

    Domain - Added string extensions

---
+++ Domain/Extensions/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace OOP_Lab_6.Domain.Extensions
+{
+    public static class StringExtension
+    {
+        public static string CleanOfNonDigits(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            string cleaned = new string(str.Where(x => char.IsDigit(x) || x == '.').ToArray());
+            return cleaned;
+        }
+
+        public static string CutOffEnd(this string str, int countOfChars)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            string cleaned = new string(str.Substring(0, str.Length - countOfChars).ToArray());
+            return cleaned;
+        }
+
+        public static string ParseHtmlCodes(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            var found = str.IndexOf("&#x27;");
+            if (found != -1)
+            {
+                str = str.Remove(found, 6).Insert(found, "\'");
+            }
+            return str;
+        }
+    }
+}
