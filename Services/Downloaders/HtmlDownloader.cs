0ffc67f59c8a7f92e2c16ca1122b80b91d085fbd
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:27:23 +0300

    Services - Added html downloaders

---
+++ Services/Downloaders/HtmlDownloader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_6.Services.Downloaders
+{
+    public class HtmlDownloader : IHtmlDownloader
+    {
+        public async Task<string> GetHtmlAsync(Uri uri)
+        {
+            var client = new HttpClient();
+            return await client.GetStringAsync(uri);
+        }
+
+        public string GetHtml(Uri uri)
+        {
+            var client = new HttpClient();
+            return client.GetStringAsync(uri).Result;
+        }
+    }
+}
---
+++ Services/Downloaders/IHtmlDownloader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_6.Services.Downloaders
+{
+    public interface IHtmlDownloader
+    {
+        Task<string> GetHtmlAsync(Uri uri);
+
+        string GetHtml(Uri uri);
+    }
+}
