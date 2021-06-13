95217e067829861ec464a5a8dd3ccec796376c5f
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:32:41 +0300

    Controllers - Implemented home contoller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OOP_Lab_6.Services.StatsParser;
+using System.Threading.Tasks;
 
 namespace OOP_Lab_6.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStatsParser _parser;
+
+        public HomeController(IStatsParser parser) => _parser = parser;
+
+        [Route("Home/StatsParallel")]
+        public async Task<IActionResult> StatsParallel()
+        {
+            dynamic model = await _parser.GetChampionStatsAsync();
+            return View("Index", model);
+        }
+
+        [Route("Home/StatsAsync")]
+        public async Task<IActionResult> StatsAsync()
+        {
+            dynamic model = await _parser.GetChampionStatsAsync();
+            return View("Index", model);
+        }
+
+        [Route("Home/Stats")]
+        public IActionResult Stats()
+        {
+            dynamic model = _parser.GetChampionStats();
+            return View("Index", model);
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return Redirect("~/Home/StatsAsync");
         }
     }
 }
