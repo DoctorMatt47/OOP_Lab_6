6d4e09b1d115da6fd89fdd32699d73c6c52b986a
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:30:44 +0300

    Domain - Added champion names parser

---
+++ Domain/Mappers/ChampionNameMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace OOP_Lab_6.Domain.Mappers
+{
+    public static class ChampionNameMapper
+    {
+        public static string GetImageName(string name)
+        {
+            if (name == "Wukong")
+                return "MonkeyKing";
+            var charsToRemove = new string[] { ".", " " };
+            foreach (var c in charsToRemove)
+            {
+                name = name.Replace(c, string.Empty);
+            }
+            if (name.Contains("\'"))
+            {
+                name = name.Replace("\'", string.Empty);
+                name = name.ToLower();
+                name = name.First().ToString().ToUpper() + name[1..];
+            }
+            var index = name.IndexOf("&");
+            if (index != -1)
+            {
+                name = name.Remove(index);
+            }
+            return name;
+        }
+    }
+}
