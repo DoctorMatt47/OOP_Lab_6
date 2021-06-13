d2f9b3b0209472526756e7f47108b5d6fb2fe636
DoctorMatt <DoctorMatt47@gmail.com>
2021-06-13 20:28:11 +0300

    Models - Added champion stats model

---
+++ Models/ChampionStats.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Lab_6.Models
+{
+    public class ChampionStats
+    {
+        public string Name { get; set; }
+        public float PickRate { get; set; }
+        public float WinRate { get; set; }
+        public float BanRate { get; set; }
+    }
+}
